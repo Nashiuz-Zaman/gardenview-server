1a6d833fc1d8ae8d7cafc8f44681db372d893d3d
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2023-11-28 19:30:25 +0600

    Add cors for current frontend local domain

--- src/middlewares/applyMiddlewares.js
+++ src/middlewares/applyMiddlewares.js
@@ -4,7 +4,12 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 const applyMiddlewares = (app) => {
-  app.use(cors());
+  app.use(
+    cors({
+      origin: true,
+      // credentials: true,
+    })
+  );
   app.use(express.json());
   app.use(cookieParser());
 };
