f66f1d5892cfd98fc2583054600cf6f70f9a16a9
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2023-11-28 23:49:29 +0600

    Create a route and its controller for google login check

---
+++ src/api/googleLoginCheck/googleLoginCheck.js
@@ -0,0 +1,32 @@
+// imports
+const User = require("../../models/User/User");
+const generateToken = require("../../utils/generateToken");
+
+const googleLoginCheck = async (req, res) => {
+  const googleUser = req.body;
+
+  // check if google user exists
+  const user = await User.findOne({ email: googleUser.email });
+  const token = generateToken({ email: googleUser.email });
+
+  console.log(user);
+
+  // if user already exists, don't save in database, just take role and create token and send them
+  if (user) {
+    return res.send({ success: true, role: user.role, token });
+  } else {
+    // if no user create the new user object in mongodb as user
+    const newGoogleUser = {
+      name: googleUser.name,
+      email: googleUser.email,
+      role: "user",
+    };
+
+    const result = await User.create(newGoogleUser);
+    if (result._id) {
+      return res.send({ success: true, role: "user", token: token });
+    }
+  }
+};
+
+module.exports = googleLoginCheck;
--- src/routes/authorization/index.js
+++ src/routes/authorization/index.js
@@ -1,11 +1,14 @@
 // imports
 const express = require("express");
 const loginRoleCheck = require("../../api/loginRoleCheck/controllers/loginRoleCheck");
+const googleLoginCheck = require("../../api/googleLoginCheck/googleLoginCheck");
 
 // create router
 const router = express.Router();
 
 // routes
 router.post("/login", loginRoleCheck);
+
+router.post("/google-login", googleLoginCheck);
 
 module.exports = router;
